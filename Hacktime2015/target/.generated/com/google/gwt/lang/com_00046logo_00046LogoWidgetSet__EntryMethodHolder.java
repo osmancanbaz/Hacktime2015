16a7ed49ff5e3404324f971e6bef43f646d82082
osman.canbaz <Osman.Canbaz@osmancanbaz.logo.com.tr>
2015-07-28 19:10:34 +0300

    sdfsfsd

---
+++ Hacktime2015/target/.generated/com/google/gwt/lang/com_00046logo_00046LogoWidgetSet__EntryMethodHolder.java
@@ -0,0 +1,6 @@
+package com.google.gwt.lang;
+public class com_00046logo_00046LogoWidgetSet__EntryMethodHolder {
+  public static final void init() {
+    // to be synthesized later
+  }
+}
---
+++ Hacktime2015/target/Hacktime2015-0.0.1-SNAPSHOT/VAADIN/widgetsets/com.logo.LogoWidgetSet/deferredjs/E8009EC97DCAD761E2917F1952586387/2.cache.js
@@ -0,0 +1,1 @@
+$wnd.com_logo_LogoWidgetSet.runAsyncCallback2('Abb(1547,1,jVd);_.uc=function fac(){iYb((!bYb&&(bYb=new nYb),bYb),this.a.d)};POd(Qh)(2);\n//# sourceURL=com.logo.LogoWidgetSet-2.js\n')
